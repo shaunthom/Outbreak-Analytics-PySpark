410ff4102c55f6e9fdde0713d9c771fa1d88a95e
Shaun Thomas <134566032+shaunthom@users.noreply.github.com>
2024-01-02 21:59:22 -0500

    Update Data.py

--- Data.py
+++ Data.py
@@ -11,3 +11,33 @@ df.show(n=5)
 
 
 #Data Preprocessing
+
+from pyspark.sql.functions import col, count, when
+zero_neg_empty_null_counts = {}
+
+for col_name in df.columns:
+    zero_neg_empty_null_counts[col_name] = df.select(
+        count(when((col(col_name) == 0) | (col(col_name) == "-") | (col(col_name) == "") | col(col_name).isNull(), col_name))
+    ).collect()[0][0]
+
+total_rows = df.count()
+
+print("Zero, Negative, Empty, and Null Counts per Column:", zero_neg_empty_null_counts)
+print("Total Number of Rows:", total_rows)
+
+from pyspark.sql.functions import col
+df = df.withColumn('Current week', when(col('Current week').isNull(), -1).otherwise(col('Current week')))
+
+flag_cols = ['Current week, flag', 'Previous 52 weeks Max, flag', 'Cumulative YTD Current MMWR Year, flag', 'Cumulative YTD Previous MMWR Year, flag']
+
+for col_name in flag_cols:
+    df = df.withColumn(col_name, when(col(col_name) == '-', 1).otherwise(0))
+
+df = df.drop('geocode', 'LOCATION1','LOCATION2','sort_order','Previous 52 week Max')    
+df = df.withColumn('Cumulative YTD Current MMWR Year', when(col('Cumulative YTD Current MMWR Year').isNull(), 0).otherwise(col('Cumulative YTD Current MMWR Year')))
+df = df.withColumn('Cumulative YTD Previous MMWR Year', when(col('Cumulative YTD Previous MMWR Year').isNull(), 0).otherwise(col('Cumulative YTD Previous MMWR Year')))
+
+#Pre-processed Data Coming up:
+
+df.show(n=50, truncate=False, vertical=True)
+df.printSchema()
