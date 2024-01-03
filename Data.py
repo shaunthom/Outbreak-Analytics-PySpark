a3f0e39fa5cca911c2860ef15539862a79d02057
Shaun Thomas <134566032+shaunthom@users.noreply.github.com>
2024-01-02 21:58:15 -0500

    Update Data.py

--- Data.py
+++ Data.py
@@ -1,1 +1,13 @@
+from pyspark.sql import SparkSession
 
+spark = SparkSession.builder \
+    .appName("NNDSSDataAnalysis") \
+    .getOrCreate()
+
+#Data Loading
+df = spark.read.csv("Desktop/Datasets/NNDSS_Weekly_Data.csv", header=True, inferSchema=True)
+df.printSchema()
+df.show(n=5)
+
+
+#Data Preprocessing
