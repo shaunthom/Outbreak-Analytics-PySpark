3ac57f048d437faf0b491791ebbf8f0d48df7727
Shaun Thomas <134566032+shaunthom@users.noreply.github.com>
2024-01-30 01:14:48 -0500

    Update Model.js

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Plot from 'react-plotly.js';
+import Papa from 'papaparse';
+
 
 const Model = () => {
   return (
